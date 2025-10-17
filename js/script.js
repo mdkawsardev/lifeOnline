8f02041a7a463da79ae135026f041d7e8ae1c2f2
mdkawsar <info.mdkawsardev@gmail.com>
2025-10-18 00:02:01 +0600

    Responsive is working...

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-let h1 = document.querySelector('.title h1');
-setInterval(() => {
- h1.classList.toggle('lightning');
-}, 2000)
+// let h1 = document.querySelector('.title h1');
+// setInterval(() => {
+//  h1.classList.toggle('lightning');
+// }, 2000)
 
 let toTopBtn = document.querySelector('.toTop');
 window.addEventListener('scroll', () => {
@@ -24,3 +24,24 @@ allLink.forEach((link) => {
         link.classList.add('active');
     })
 })
+
+window.addEventListener('scroll', function() {
+    const sections = document.querySelectorAll('.section');
+    const navbarLinks = Array.from(document.querySelectorAll('.link'));
+
+    let currentSection = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - window.innerHeight / 3;
+        if ((window.scrollY >= sectionTop) && (window.scrollY < sectionTop + section.offsetHeight)) {
+            currentSection = section.getAttribute('id');
+        }
+    });
+
+    navbarLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href').substring(1) === currentSection) {
+            link.classList.add('active');
+        }
+    });
+});
